d227a560bdf10219ab5d8fcced4a8029510b8f61
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-06-06 17:13:23 +0530

    added new packages and logo

--- lib/Screens/Splash_Screen.dart
+++ lib/Screens/Splash_Screen.dart
@@ -1,67 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-import 'Calendar2.dart';
-
-class Sp_Screen extends StatelessWidget {
-  const Sp_Screen({Key? key}) : super(key: key);
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Splash_Screen(),
-    );
-  }
-}
-
-class Splash_Screen extends StatefulWidget {
-  const Splash_Screen({Key? key}) : super(key: key);
-
-  @override
-  State<Splash_Screen> createState() => _Splash_ScreenState();
-}
-
-class _Splash_ScreenState extends State<Splash_Screen>
-    with SingleTickerProviderStateMixin {
-  @override
-  void initState() {
-    super.initState();
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
-    Future.delayed(Duration(seconds: 5), () {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => Calendar_Widget()),
-      );
-    });
-  }
-
-  @override
-  void dispose() {
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
-        overlays: SystemUiOverlay.values);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: double.infinity,
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(
-          colors: [Colors.blue, Colors.pink],
-          begin: Alignment.topRight,
-          end: Alignment.bottomLeft,
-        )),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Calendar',
-              style: TextStyle(fontSize: 40, color: Colors.white),
-            )
-          ],
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
