4b3807dfcb86c13de85f76dde730cd8d33ca9380
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-06-19 19:12:08 +0530

    main changed

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,19 @@ import 'package:calendar_navindu/Calendar.dart';
 import 'package:calendar_navindu/Screens/Calendar2.dart';
 import 'package:calendar_navindu/Screens/Splash_Screen.dart';
 import 'package:calendar_navindu/Screens/Test_Dashboard.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'package:flutter/material.dart';
 
-void main() {
+/*void main() {
+  runApp(SplashScreen());
+}*/
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(SplashScreen());
 }
